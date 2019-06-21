f326bd7a1d90ccd3c236f66cae13fc31798ed4d4
Boyko Anton <me@boykoant.pro>
2019-06-21 19:35:26 +0300

    continued working on demo app

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 (function() {
+    var configs = {
+        storageName: '',
+        storageKey: '',
+        storageContainer: 'faces'
+    };
+
     var videoPreview = document.getElementById('videoPreview');
     var imgPrevew = document.getElementById('imgPreview');
 
     var Webcam = require('webcamjs');
     Webcam.attach(videoPreview);
 
-    document.getElementById('btnSave').addEventListener('click', getPhotoFromCamera);
+    var storage = require('azure-storage');
+    var blobService = storage.createBlobService(configs.storageName, configs.storageKey);
+    blobService.createContainerIfNotExists(configs.storageContainer, {
+        publicAccessLevel: 'blob'
+    }, function(error, result, response) {
+        if (!error) {
+            // if result = true, container was created.
+            // if result = false, container already existed.
+        }
+    });
+
+
+    document.getElementById('btnCapture')
+        .addEventListener('click', capturePhotoFromCamera);
 
     navigator.getUserMedia({ video: true, audio: false }, (localMediaStream) => {
         videoPreview.srcObject = localMediaStream
         videoPreview.autoplay = true
-     }, (e) => { });
+    }, (e) => { });
 
-     function getPhotoFromCamera() {
+    function capturePhotoFromCamera() {
         Webcam.snap(function(data){
             imgPrevew.setAttribute('src', data);
         });
-     }
+    }
 })();
